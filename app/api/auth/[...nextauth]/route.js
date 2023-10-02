0cf166897dd8b00ac1f7b4667ac2a8bbd941119f
musabbir-m <musabbir313@gmail.com>
2023-10-02 11:12:03 +0600

    setup nextauth and user model

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
 
+import User from '@models/user'
 import { connectToDB } from "@utils/database";
+import Profile from "@components/Profile";
 
 
 const handler= NextAuth(
@@ -12,15 +14,33 @@ const handler= NextAuth(
                 clientSecret:process.env.GOOGLE_CLEINT_SECRET,
             })
         ],
+        
+        //tracking user session
         async session({session}){
+         const sessionUser= await User.findOne({
+            email:session.user.email
+         })
 
+         session.user.id= sessionUser._id.toString();
+         return session;
         },
-        async signIn({proifle}){
+
+        async signIn({profile}){
             try{
                 await connectToDB()
                 //check if user alredy created
-
+                const userExists= await User.findOne({
+                    email: profile.email
+                })
                 //if not create new user
+                if(!userExists) {
+                    await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture 
+
+                    })
+                }
             } 
             catch(error){
 
